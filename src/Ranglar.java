b45644fd082e2e154da865b67f8ead167f660b7d
Nodirbek8008 <63007781+Nodirbek8008@users.noreply.github.com>
2024-11-06 20:47:56 +0500

    Ranglar v1.0

---
+++ src/Ranglar.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Ranglar {
+    public static void main(String[] args) {
+        ArrayList<String> ranglar = new ArrayList<>();
+        ranglar.add("Qizil");
+        ranglar.add("Yashil");
+        ranglar.add("Ko'k");
+        ranglar.add("Sariq");
+        ranglar.add("Oq");
+        ranglar.add("Qora");
+        ranglar.add("Binafsha");
+
+        System.out.println("Ranglar to'plami: " + ranglar);
+    }
+}
